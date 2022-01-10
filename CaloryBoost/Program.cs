411c1b9bf709a6ca37b40c495430a90200dc395d
Münir Taner Güler <munirtaner.guler@bilgeadamboost.com>
2022-01-10 21:58:56 +0300

    program.cs editted

--- CaloryBoost/Program.cs
+++ CaloryBoost/Program.cs
@@ -14,6 +14,7 @@ namespace CaloryBoost
         [STAThread]
         static void Main()
         {
+            //taner deneme
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
